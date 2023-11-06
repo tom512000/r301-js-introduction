b066ceda40f8811db2ac65604865ec5e3b360949
siko0001 <tom.sikora@etudiant.univ-reims.fr>
2023-11-06 08:51:50 +0100

    7. Somme d'un tableau d'entier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,25 +2,24 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    "jest/globals": true,
   },
-  extends: 'airbnb-base',
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
-  ],
+  extends: ["airbnb-base", "prettier"],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
+  plugins: ["prettier", "jest"],
   rules: {
+    "prettier/prettier": [
+      "warn",
+      {
+        singleQuote: false,
+        trailingComma: "all",
+        tabWidth: 2,
+      },
+    ],
+    "import/prefer-default-export": ["off"],
   },
 };
--- src/introduction.js
+++ src/introduction.js
@@ -1,7 +1,19 @@
-export function add(a, b) {
+export function add(a, b)
+{
   return a + b;
 }
 
-export function sub(a, b) {
+export function sub(a, b)
+{
   return b - a;
+}
+
+export function sum(values)
+{
+  let resultat = 0;
+  for (var i = 0; i < values.length; i++) {
+    resultat += values[i];
+  }
+
+  return resultat;
 }
