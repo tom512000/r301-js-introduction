b249cffe6d3c58a90a08c4de00ff64e1c94b365d
siko0001 <tom.sikora@etudiant.univ-reims.fr>
2023-11-13 10:23:33 +0100

    21. Finalisation de l'affichage des utilisateurs

--- src/users-ui.js
+++ src/users-ui.js
@@ -1,10 +1,35 @@
 export function createHtmlUser(age) {
     const liElt = document.createElement("li");
     liElt.className = "user";
-    liElt.innerHTML = age;
+
+    const nameUser = document.createElement("input");
+    nameUser.className = "input user__name";
+    nameUser.type = "text";
+    nameUser.value = "name";
+    liElt.appendChild(nameUser);
+
+    liElt.append(" - ");
+
+    const ageUser = document.createElement("input");
+    ageUser.className = "input user__age";
+    ageUser.type = "number";
+    ageUser.value = age;
+    ageUser.min = "1";
+    liElt.appendChild(ageUser);
+
+    const DeleteButton = document.createElement("button");
+    DeleteButton.className = "user__delete";
+    DeleteButton.type = "button";
+    liElt.appendChild(DeleteButton);
+
+    const spanButton = document.createElement("span");
+    spanButton.className = "material-symbols-outlined";
+    spanButton.innerHTML = "delete";
+    DeleteButton.appendChild(spanButton);
+
     return liElt;
 }
 
 export function setAddUserEltCallback(addUserElt, usersElt) {
-    addUserElt.addEventListener('click', () => { usersElt.appendChild(createHtmlUser(usersElt)) });
+    addUserElt.addEventListener('click', () => { usersElt.appendChild(createHtmlUser(Math.floor(Math.random() * (32 - 12) + 12))) });
 }
